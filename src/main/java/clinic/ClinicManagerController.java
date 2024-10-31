90a08676248816d6dcf5dc82227d93bbf914d3b6
scarletrat <gl548@scarletmail.rutgers.edu>
2024-10-31 12:41:32 -0400

    scene builder

--- src/main/java/clinic/ClinicManagerController.java
+++ src/main/java/clinic/ClinicManagerController.java
@@ -1,7 +1,13 @@
 package clinic;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class ClinicManagerController {
     @FXML
@@ -10,5 +16,20 @@ public class ClinicManagerController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    /**
+     *Load provider list to file.
+     */
+    void loadProviders(ActionEvent event) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open Source File for the Import");
+        chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
+                new ExtensionFilter("All Files", "*.*"));
+        Stage stage = new Stage();
+        File sourceFile = chooser.showOpenDialog(stage); //get the reference of the source file
+        ///Scanner scanner = new Scanner(sourceFile);
+        //write code to read from the file.
     }
 }
--- src/main/java/clinic/ClinicManagerMain.java
+++ src/main/java/clinic/ClinicManagerMain.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class ClinicManagerMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ClinicManagerMain.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ClinicManagerMain.class.getResource("clinic-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("Clinic Manager - Scheduling Office/Imaging Appointments!");
         stage.setScene(scene);
